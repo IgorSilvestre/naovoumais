74f5d9c46b8f7c9af4bb7da745309675a6574415
Igor Silvestre <77891078+IgorSilvestre@users.noreply.github.com>
2022-10-26 20:42:15 -0300

    removed unused tag

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ function App() {
             <h1 className={'text-3xl font-bold text-blue-600'}>
                 Install & Setup Vite + React + Typescript + Tailwind CSS 3
             </h1>
-            <ProductCard 
         </div>
     )
 }
